6e4b6c60f11a2bd95acc707234ad24202914db2f
NailSacul <ianlucaschaves@gmail.com>
2023-11-29 00:09:18 -0300

    Project created

---
+++ src/main/java/com/naisacul/challengebackenduber/ChallengeBackendUberApplication.java
@@ -0,0 +1,13 @@
+package com.naisacul.challengebackenduber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBackendUberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBackendUberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naisacul/challengebackenduber/ChallengeBackendUberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naisacul.challengebackenduber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBackendUberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
